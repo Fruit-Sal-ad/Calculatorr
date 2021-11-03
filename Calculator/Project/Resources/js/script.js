b76a2afe8e8b638d3d74fad9408ca97b9c57a8ab
Fruit-Sal-ad <90779611+Fruit-Sal-ad@users.noreply.github.com>
2021-11-03 11:21:08 -0400

    Add files via upload

---
+++ Calculator/Project/Resources/js/script.js
@@ -0,0 +1,31 @@
+document.getElementById("1").onclick = function write1()
+  {
+    document.getElementById("screen").innerHTML = "1";
+  }
+
+document.getElementById("clear").onclick = function writeClear()
+{
+  document.getElementById("screen").innerHTML = " ";
+}
+
+document.getElementById("num2").onclick = function write1()
+  {
+    document.getElementById("screen").innerHTML = "2";
+  }
+
+  document.getElementById("num3").onclick = function write1()
+    {
+      document.getElementById("screen").innerHTML = "3";
+    }
+
+
+    document.getElementById("num4").onclick = function write1()
+      {
+        document.getElementById("screen").innerHTML = "4";
+      }
+
+
+      document.getElementById("num5").onclick = function write1()
+        {
+          document.getElementById("screen").innerHTML = "5";
+        }
